b75eb8e54bc85be5f62e804494f02e96bfa8791b
haniehm26 <haniehmahdavi26@gmail.com>
2024-12-04 00:26:46 +0100

    added proxy_list

--- webscraping/code/constants.py
+++ webscraping/code/constants.py
@@ -1,5 +1,20 @@
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+PROXY_LISTS = [
+    "185.17.141.130:3128",
+    "193.106.128.49:1080",
+    "91.213.197.120:80",
+    "37.123.21.42:3128",
+    "81.93.242.129:8080",
+    "51.15.26.151:3128",
+    "85.195.122.202:443",
+    "94.177.211.99:8080",
+    "159.89.133.100:8080",
+    "45.76.102.244:8888",
+]
 
-BOOK_IDS_FILE = "PATH/fds_project/webscraping/book32-listing.csv"
+BOOK_IDS_FILE = "PATH/fds_project/webscraping/book30-listing-train.csv"
 
-BOOK_DESCRIPTION_FILE = "../data/book_descriptions.csv"
+BOOK_DESCRIPTION_FILE = "../data/book_descriptions_train_01.csv"
+
+START = 17
+END = 500
+MAX_WORKERS = 1
